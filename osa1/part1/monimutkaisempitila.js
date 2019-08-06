2bf0495a2c62e01959ac731699bbd7d634da0d76
kimmojans <53338959+kimmojans@users.noreply.github.com>
2019-08-06 15:25:22 +0300

    Update monimutkaisempitila.js
    Yhteen olioon

--- osa1/part1/monimutkaisempitila.js
+++ osa1/part1/monimutkaisempitila.js
@@ -4,22 +4,32 @@ import ReactDOM from 'react-dom';
 /* 1D - MONIMUTKAISEMPI TILA, REACTIN DEBUGGAUS */
 
 const App = (props) => {
-    const [ left, setLeft ] = useState(0)
-    const [ right, setRight ] = useState(0)
+    const [ clicks, setClicks ] = useState({left: 0, right: 0})
+    
 
- 
+ const handleLeftClick = () => {
+     const newClicks = {
+         left: clicks.left + 1,
+         right: clicks.right
+     }
+     setClicks(newClicks)
+ }
+
+ const handleRightClick = () => {
+    const newClicks = {
+        left: clicks.left,
+        right: clicks.right +1
+    }
+    setClicks(newClicks)
+}
 
     return (
         <div>
             <div>
-                {left}
-                <button onClick={() => setLeft(left + 1)}>
-                left
-                </button>
-                <button onClick={() => setRight(right + 1)}>
-                right
-                </button>
-                {right}
+                {clicks.left}
+                <button onClick={handleLeftClick}>left</button>
+                <button onClick={handleRightClick}>right</button>
+                {clicks.right}
             </div>
         </div>
     )
