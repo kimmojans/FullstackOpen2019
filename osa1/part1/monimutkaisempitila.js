6d7b087ad0aeee1253b7061d9f27a937082ca7d1
kimmojans <53338959+kimmojans@users.noreply.github.com>
2019-08-06 15:19:49 +0300

    Create monimutkaisempitila.js
    Ensimmäinen versio

---
+++ osa1/part1/monimutkaisempitila.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+
+/* 1D - MONIMUTKAISEMPI TILA, REACTIN DEBUGGAUS */
+
+const App = (props) => {
+    const [ left, setLeft ] = useState(0)
+    const [ right, setRight ] = useState(0)
+
+ 
+
+    return (
+        <div>
+            <div>
+                {left}
+                <button onClick={() => setLeft(left + 1)}>
+                left
+                </button>
+                <button onClick={() => setRight(right + 1)}>
+                right
+                </button>
+                {right}
+            </div>
+        </div>
+    )
+
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
